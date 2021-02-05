f3e8083d3280b43572ccc67cd8e0fef6e9d9ac61
thalys_chenhao <chenhao@thalys.net.cn>
2021-02-05 09:45:35 +0800

    添加core

---
+++ core/src/main/java/com/busycount/core/CoreApp.kt
@@ -0,0 +1,9 @@
+package com.busycount.core
+
+/**
+ * @author : thalys_ch
+ * Date : 2021/02/05
+ * Describe :
+ **/
+class CoreApp {
+}
---
+++ core/src/main/java/com/busycount/core/ui/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.busycount.core.ui
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @author : thalys_ch
+ * Date : 2021/02/05
+ * Describe :
+ **/
+abstract class BaseActivity : AppCompatActivity() {
+}
